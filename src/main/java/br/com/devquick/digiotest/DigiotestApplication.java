ef3eb81cbf897ecc14b38f692c06a27fa1ebf48c
Warley Rocha <wsrochab@gmail.com>
2024-08-22 18:53:26 -0300

    Initial commit project generated by Spring Initializr

---
+++ src/main/java/br/com/devquick/digiotest/DigiotestApplication.java
@@ -0,0 +1,13 @@
+package br.com.devquick.digiotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiotestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devquick/digiotest/DigiotestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devquick.digiotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
